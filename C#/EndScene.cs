ef3e44d89a5fb204e1a3c11dcce8c98f4733dc6a
Ioannis Kalaitzidis <120419392+Kalaitzon@users.noreply.github.com>
2025-01-01 20:11:32 +0200

    Add files via upload

---
+++ C#/EndScene.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using TMPro;
+using System.Runtime.InteropServices;
+
+public class EndScene : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void SendScoreToServer(int score);
+
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    private ScoreKeeper scoreKeeper;
+    private bool scoreSent = false; // Flag για να διασφαλίσουμε ότι η μέθοδος καλείται μόνο μία φορά
+
+    void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        float scorePercentage = scoreKeeper.CalculateScore(); // Υπολογισμός του ποσοστού επίδοσης
+        string message = "";
+
+        if (scorePercentage >= 85)
+        {
+            message = "Συγχαρητήρια! Εξαιρετική απόδοση!";
+        }
+        else if (scorePercentage >= 70)
+        {
+            message = "Μπράβο! Πολύ καλή δουλειά!";
+        }
+        else if (scorePercentage >= 55)
+        {
+            message = "Καλή προσπάθεια! Μπορείς και καλύτερα!";
+        }
+        else
+        {
+            message = "Χρειάζεται περισσότερη δουλειά. Μην τα παρατάς!";
+        }
+
+        finalScoreText.text = message + "\nΤο score σου είναι: " + scorePercentage + "%";
+
+        // Κλήση για αποστολή του σκορ στον server
+        SendScoreToFirebase(Mathf.RoundToInt(scorePercentage));
+    }
+
+    public void SendScoreToFirebase(int scorePercentage)
+    {
+        if (scoreSent) return; // Έλεγχος αν το score έχει ήδη σταλεί για αποφυγή διπλής αποστολής
+        scoreSent = true; // Flag ώστε να αποφευχθεί η επαναλαμβανόμενη αποστολή
+
+        // Καταγραφή του score στο Debug log
+        Debug.Log($"Sending score: {scorePercentage}");
+
+        #if UNITY_WEBGL && !UNITY_EDITOR
+            SendScoreToServer(scorePercentage); // Κλήση της συνάρτησης αποστολής στον server σε περιβάλλον WebGL
+        #else
+            Debug.Log("SendScoreToServer can only be called in WebGL builds."); // Εμφάνιση μηνύματος στο Debug log αν η πλατφόρμα δεν είναι WebGL
+        #endif
+    }
+}
---
+++ C#/Timer.cs
@@ -0,0 +1,57 @@
+// Ioannis Kalaitzidis, 2120067, Thesis
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 15f;                        // Χρόνος για την απάντηση στην ερώτηση
+    [SerializeField] float timeToShowcorrectAnswer = 5f;                      // Χρόνος για την εμφάνιση της σωστής απάντησης/επιβράβευσης
+
+    public bool loadNextQuestion;                                               // Έλεγχος για τη φόρτωση της επόμενης ερώτησης
+    public float fillFraction;                                                  // Κλάσμα που χρησιμοποιείται για την ενδεικτική γραμμή χρονοδιαγράμματος
+    public bool isAnsweringQuestion;                                            // Κατάσταση απάντησης ερώτησης
+    float timerValue;                                                           // Τιμή του τρέχοντος χρονομέτρου
+
+    void Update()
+    {
+        UpdateTimer();                                                          // Ενημέρωση του χρονομέτρου σε κάθε καρέ
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0;                                                         // Μηδενίζει το χρονόμετρο
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;                                           // Μειώνει το χρονόμετρο βάσει του χρόνου που πέρασε
+
+        if (isAnsweringQuestion)
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;             // Υπολογίζει το κλάσμα για την εμφάνιση
+            }
+            else
+            {
+                isAnsweringQuestion = false;                                    // Τερματίζει την περίοδο απάντησης
+                timerValue = timeToShowcorrectAnswer;                           // Ορίζει τον χρόνο για την εμφάνιση της σωστής απάντησης
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowcorrectAnswer;            // Υπολογίζει το κλάσμα για την εμφάνιση της σωστής απάντησης
+            }
+            else
+            {
+                isAnsweringQuestion = true;                                     // Επανέρχεται στην περίοδο απάντησης
+                timerValue = timeToCompleteQuestion;                            // Ορίζει τον χρόνο για την επόμενη ερώτηση
+                loadNextQuestion = true;                                        // Ενεργοποιεί τη φόρτωση της επόμενης ερώτησης
+            }
+        }
+    }
+}
